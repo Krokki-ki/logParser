b121ebd9e75e3f984f964d3d527969632f27395f
AnatolySvetaev <svetaev.anatoly@yandex.ru>
2025-03-06 18:00:56 +0300

    В файл run.py добавлена функция continue_work() для возможности возврата к началу программы. В предыдущей версии программа закрывалась по завершению полного цикла обработки. В текущей версии программа будет запрашивать пользователя о возврате к началу программы, либо о завершении работы. Пользовательский ввод '1' вернёт программу в стартовое состояние, а ввод '2' завершит программу. Возвращаться к началу программы можно сколько угодно раз, поскольку дял прерывания бесконечного цикла требуется конкретный ввод пользователя

--- run.py
+++ run.py
@@ -15,9 +15,26 @@ def run():
     else:
         print("Неверный выбор. Программа завершена.")
 
+def continue_work():
+     while True: # Бесконечный цикл для многократного возврата в начало программы
+         print("\nЧтобы вернуться в начало программы, нажмите 1")
+         print("Чтобы завершить программу, нажмите 2")
+
+         choice_continue = input("Укажите ваш выбор: ")
+
+         if choice_continue == "1":
+             return True
+         elif choice_continue == "2":
+             print("Программа завершена.")
+             return False
+         else:
+             print("Неверный выбор. Программа завершена.")
+
+
 
 
 if __name__ == "__main__":
-    run()
-
-input("Нажмите Enter для выхода...")
+    while True:     # Основной цикл программы
+        run()
+        if not continue_work():
+            break   # Выход из цикла при выборе "2"
